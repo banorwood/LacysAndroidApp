5a4b7a31618701761579d4be5557fa8b908ccd02
banorwood <bnorwood1@yahoo.com>
2015-03-04 19:27:59 -0500

    Added a basic login screen

--- Lacys/app/src/main/java/com/lacys/SignInScreen.java
+++ Lacys/app/src/main/java/com/lacys/SignInScreen.java
@@ -1,10 +1,51 @@
 package com.lacys;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * Created by Christina on 2/14/2015.
- */
 public class SignInScreen extends Activity {
+
+    private EditText  username=null;
+    private EditText  password=null;
+    private TextView attempts;
+    private Button login;
+    int counter = 3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signin_main);
+        username = (EditText)findViewById(R.id.editText1);
+        password = (EditText)findViewById(R.id.editText2);
+        attempts = (TextView)findViewById(R.id.textView5);
+        attempts.setText(Integer.toString(counter));
+        login = (Button)findViewById(R.id.button1);
+    }
+
+    public void login(View view){
+        if(username.getText().toString().equals("admin") &&
+                password.getText().toString().equals("admin")){
+            Toast.makeText(getApplicationContext(), "Redirecting...",
+                    Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Wrong Credentials",
+                    Toast.LENGTH_SHORT).show();
+            attempts.setBackgroundColor(Color.RED);
+            counter--;
+            attempts.setText(Integer.toString(counter));
+            if(counter==0){
+                login.setEnabled(false);
+            }
+
+        }
+
+    }
+
 
 }
