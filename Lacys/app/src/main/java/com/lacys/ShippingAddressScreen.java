dd70fac2e77f41d85fa87ea5c6331bbaa5cbee7a
banorwood <bnorwood1@yahoo.com>
2015-03-25 17:54:35 -0400

    1. Edited layout of Sign In screen

---
+++ Lacys/app/src/main/java/com/lacys/ShippingAddressScreen.java
@@ -0,0 +1,16 @@
+package com.lacys;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by blakenorwood on 3/24/15.
+ */
+public class ShippingAddressScreen extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.shipping_address_screen);
+    }
+}
